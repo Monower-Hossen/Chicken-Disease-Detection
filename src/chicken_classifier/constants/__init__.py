f86b324aec2be88b009882052cab7744f2a40a6e
Monower-Hossen <monower.cse@gmail.com>
2026-05-16 00:13:55 +0600

    Refactor constants: update file paths to use absolute paths based on project root

--- src/chicken_classifier/constants/__init__.py
+++ src/chicken_classifier/constants/__init__.py
@@ -1,4 +1,11 @@
+import os
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+# Get the absolute path of the current file
+CURRENT_FILE_PATH = Path(__file__).resolve()
+
+# Navigate up to the project root directory
+PROJECT_ROOT = CURRENT_FILE_PATH.parents[3] # src/chicken_classifier/constants/__init__.py -> src/chicken_classifier/constants -> src/chicken_classifier -> src -> Chicken-Disease-Detection (project root)
+
+CONFIG_FILE_PATH = Path(os.path.join(PROJECT_ROOT, "config", "config.yaml"))
+PARAMS_FILE_PATH = Path(os.path.join(PROJECT_ROOT, "params.yaml"))
